e040da51b873fbb2701df370465081add91ac53e
Xavier Marques <xavierpmarques@gmail.com>
2021-06-16 01:00:45 +0100

    generate navigation bar with javascript

--- src/index.js
+++ src/index.js
@@ -1,1 +1,40 @@
-alert("running");
+import "./style.scss";
+import { populateMenu } from '/src/menu.js';
+
+
+document.addEventListener("DOMContentLoaded", function () {
+const header = document.querySelector('#header');
+const content = document.querySelector('#content');
+
+const populateNavigationBar = () => {
+    // Create List For Navigation Bar
+    const navigationBar = document.createElement('ul');
+    // Navigation Bar Options
+    const home = document.createElement('li');
+    home.innerText = "Home";
+    home.classList.add('menu');
+    // home.addEventListener('click', populateHome);
+
+    const menu = document.createElement('li');
+    menu.innerText = "Menu";
+    menu.classList.add('menu');
+    menu.addEventListener('click', populateMenu);
+
+    const contact = document.createElement('li');
+    contact.innerText = "Contact";
+    contact.classList.add('menu');
+    // contact.addEventListener('click', populateContact);
+
+    // Append Options to Navigation Bar List
+    navigationBar.appendChild(home);
+    navigationBar.appendChild(menu);
+    navigationBar.appendChild(contact);
+    // Append navigationBar to header
+    header.appendChild(navigationBar);
+}
+
+populateNavigationBar();
+
+});
+
+
