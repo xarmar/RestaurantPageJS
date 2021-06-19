1519bd11d8e3908d23dfa9e82396d90694e25acb
Xavier Marques <xavierpmarques@gmail.com>
2021-06-20 00:56:34 +0100

    webpack config update

--- webpack.config.js
+++ webpack.config.js
@@ -1,9 +1,46 @@
 const path = require('path');
+const HtmlWebpackPlugin = require('html-webpack-plugin');
 
 module.exports = {
-    entry: './src/index.js',
+    mode: 'development',
+    entry: {
+        main: './src/index.js',
+        home: './src/home.js',
+        menu: './src/menu.js',
+        contact: './src/contact.js',
+        helperFunctions: './src/helperFunctions.js',
+    },
+    devtool: 'inline-source-map',
     output: {
-        filename: 'main.js',
+        filename: '[name].bundle.js',
         path: path.resolve(__dirname, 'dist'),
+        clean: true,
+    },
+    plugins: [
+        new HtmlWebpackPlugin({
+          title: 'Cocktails & Dreams',
+          // Load a custom template (lodash by default)
+          template: './src/index.html',
+        }),
+      ],
+    module: {
+        rules: [
+            {
+                test: /\.s[ac]ss$/i,
+                use: [
+                    'style-loader',
+                    'css-loader',
+                    'sass-loader'
+                ],
+            },
+            {
+                test: /\.(png|svg|jpg|jpeg|gif)$/i,
+                type: 'asset/resource',
+            },
+            {
+                test: /\.html$/i,
+                loader: 'html-loader',
+            },
+        ],
     },
 };
